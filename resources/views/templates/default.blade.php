9d48bd27434c8ae07b8dab24866b0fa15a512249
jomaf1010 <fontanillajosephjohn@gmail.com>
2016-05-26 13:11:56 +0800

    [SETUP] Task runner and initial required files

---
+++ resources/views/templates/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+@include('partials.head_markup')
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+</body>
+<script src="/assets/vendor.js"></script>
+<script src="/assets/app.js"></script>
+</html>
